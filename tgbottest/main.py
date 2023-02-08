1b8de0e5567d054b4ca57bea336c9e65393ca77e
Zerderr <114272076+Zerderr@users.noreply.github.com>
2023-02-08 20:06:57 +0500

    Update main.py

--- tgbottest/main.py
+++ tgbottest/main.py
@@ -3,17 +3,10 @@ import logging
 from aiogram import Bot, Dispatcher, types
 
 from handlers import router
-
-# Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
-# Объект бота
 bot = Bot(token="notokenhere:D")
-# Диспетчер
 dp = Dispatcher()
 
-
-
-# Запуск процесса поллинга новых апдейтов
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
